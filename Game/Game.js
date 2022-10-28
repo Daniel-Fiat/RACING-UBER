80b2935b991219336234eace4ff5dac0123156e5
Larisa Mancebo <larisa.mancebo@gmail.com>
2022-10-28 16:56:31 +0200

    game modified

--- Game/Game.js
+++ Game/Game.js
@@ -1,36 +1,37 @@
 const Game = {
 
-    FPS:60,
-    canvas:undefined,
-    ctx:undefined,
-    width:undefined,
+    FPS: 60,
+    canvas: undefined,
+    ctx: undefined,
+    width: undefined,
     height: undefined,
     backgroundImg: undefined,
-    
 
-init(){
-    this.canvas = document.querySelector(".canvas")
-    this.ctx = this.canvas.getContext("2d")
-    
+
+    init() {
+        this.canvas = document.querySelector(".canvas")
+        this.ctx = this.canvas.getContext("2d")
+
         this.width = window.innerWidth
         this.height = window.innerHeight
 
         this.canvas.width = this.width
         this.canvas.height = this.height
-        
-        this.start()    
-},
 
-start(){ 
-    this.intervalID = setInterval(() => {
-   
-    this.backgroundImg= new Image()
-    this.backgroundImg.src= "../Img/Mapa.png"
-    this.ctx.drawImage(this.backgroundImg, 0, 0, this.width, this.height)
+        this.start()
+    },
 
-    }, 1000/this.FPS);
+    start() {
+        this.intervalID = setInterval(() => {
 
-}
+            this.backgroundImg = new Image()
+            this.backgroundImg.src = "../Img/Mapa.png"
+            this.ctx.drawImage(this.backgroundImg, 0, 0, this.width, this.height)
+
+            console.log('HOOLIIIIIIWIIIIII')
+        }, 1000 / this.FPS);
+
+    }
 
 
 
