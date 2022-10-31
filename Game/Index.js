c1d61a012da222da123ad550bb9ce82bfeb9f5d6
Daniel-Fiat <danielfiatcasado@gmail.com>
2022-10-31 17:35:35 +0100

    1735

--- Game/Index.js
+++ Game/Index.js
@@ -1,12 +1,13 @@
 onload = () => {
-    Game.init(1, "blue", "red", 1, 2)// (map,player1color,player2color,playerkey,playerkey)
+    //Game.init(1, "blue", "red", 1, 2)// (map,player1color,player2color,playerkey,playerkey)
     const startGame = document.querySelector('.startGame')
     const canvas = document.querySelector('#canvas')
+    const introPage = document.querySelector('.introPage')
 
     startGame.addEventListener('click', () => {
         introPage.classList.toggle('nonDisplay')
         canvas.classList.toggle('nonDisplay')
-        Game.init()
+        Game.init(1, "blue", "red", 1, 2)
     })
 }
 
