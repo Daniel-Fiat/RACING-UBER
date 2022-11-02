98a43694d7ebc37eb1db4e306cd3b8bc52d82157
Larisa Mancebo <larisa.mancebo@gmail.com>
2022-11-02 12:03:01 +0100

    yesterday's changes

--- Game/Index.js
+++ Game/Index.js
@@ -3,8 +3,13 @@ onload = () => {
     const startGame = document.querySelector('.startGame')
     const GameScreen = document.querySelector('.GameScreen')
     const introPage = document.querySelector('.introPage')
-    
-    
+    const playerOne = document.querySelector('#player1') //input nombre
+    const playerTwo = document.querySelector('#player2') //input nombre
+    const blue = document.querySelector('#carBlue') // input color coche
+    const red = document.querySelector('#carRed') // input color coche
+    const white = document.querySelector('#carWhite') // input color coche
+
+
 
     startGame.addEventListener('click', () => {
         introPage.classList.toggle('nonDisplay')
