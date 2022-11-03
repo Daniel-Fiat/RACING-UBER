0eba168e11bfb1317108c28dadbed404672ed016
Larisa Mancebo <larisa.mancebo@gmail.com>
2022-11-03 12:19:26 +0100

    3-11 changes

--- Game/Index.js
+++ Game/Index.js
@@ -3,31 +3,33 @@ onload = () => {
     const startGame = document.querySelector('.startGame')
     const GameScreen = document.querySelector('.GameScreen')
     const introPage = document.querySelector('.introPage')
-    const playerOne = document.querySelector('#player1') //input nombre
-    const playerTwo = document.querySelector('#player2') //input nombre
-    const blue = document.querySelector('#carBlue') // input color coche
-    const red = document.querySelector('#carRed') // input color coche
-    const white = document.querySelector('#carWhite') // input color coche
-    const green = document.querySelector('#carGreen')
-    const orange = document.querySelector('#carOrange')
+    const blue1 = document.querySelector('#carBlue1')
+    const red1 = document.querySelector('#carRed1')
+    const white1 = document.querySelector('#carWhite1')
+    const green1 = document.querySelector('#carGreen1')
+    const orange1 = document.querySelector('#carOrange1')
+    const royalBlue1 = document.querySelector('#carRoyalBlue1')
+    const blue2 = document.querySelector('#carBlue2')
+    const red2 = document.querySelector('#carRed2')
+    const white2 = document.querySelector('#carWhite2')
+    const green2 = document.querySelector('#carGreen2')
+    const orange2 = document.querySelector('#carOrange2')
+    const royalBlue2 = document.querySelector('#carRoyalBlue2')
 
-    
-    
-    
     startGame.addEventListener('click', () => {
         SetColorCar()
         introPage.classList.toggle('nonDisplay')
         canvas.classList.toggle('nonDisplay')
-        //let Player1Color= ??
-        // Player2Color= ??
-    
-        const map= 1
-        const Player1SetKey=1
-        const Player2SetKey=2
-        
+        // let player1Color =
+        // let player2Color = 
 
-        Game.init(map,"red", "blue", Player1SetKey, Player2SetKey)
-        
+        const map = 1
+        const Player1SetKey = 1
+        const Player2SetKey = 2
+
+
+        Game.init(map, "red", "blue", Player1SetKey, Player2SetKey)
+
     })
 
 }
